279c34736723993dcf65d3f8601b8b6cb0152ee3
Ahmed <101567711+AhmedBSF@users.noreply.github.com>
2023-08-20 15:39:25 +0300

    fixed test issues and complete user model

--- src/models/tests/user_spec.ts
+++ src/models/tests/user_spec.ts
@@ -7,8 +7,47 @@ describe("UserModel", () => {
     expect(model.index).toBeDefined();
   });
 
+  it("should have a show method", () => {
+    expect(model.show).toBeDefined();
+  });
+
+  it("should have a create method", () => {
+    expect(model.create).toBeDefined();
+  });
+
+  it("create method should add a user", async () => {
+    const result = await model.create({
+      first_name: "John",
+      last_name: "Doe",
+      password: "password",
+    });
+    expect(result).toEqual({
+      id: 1,
+      first_name: "John",
+      last_name: "Doe",
+      password: "password",
+    });
+  });
+
   it("index method should return a list of users", async () => {
     const result = await model.index();
-    expect(result).toEqual([]);
+    expect(result).toEqual([
+      {
+        id: 1,
+        first_name: "John",
+        last_name: "Doe",
+        password: "password",
+      },
+    ]);
+  });
+
+  it("show method should return the correct user", async () => {
+    const result = await model.show(1);
+    expect(result).toEqual({
+      id: 1,
+      first_name: "John",
+      last_name: "Doe",
+      password: "password",
+    });
   });
 });
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,7 +1,7 @@
 import Client from "../database";
 
 export type User = {
-  id: string;
+  id?: number;
   first_name: string;
   last_name: string;
   password: string;
@@ -17,6 +17,37 @@ export class UserModel {
       return result.rows;
     } catch (err) {
       throw new Error(`Cannot get users ${err}`);
+    }
+  }
+
+  async show(id: number): Promise<User> {
+    try {
+      const conn = await Client.connect();
+      const sql = "SELECT * FROM users WHERE id=($1)";
+      const result = await conn.query(sql, [id]);
+      conn.release();
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Could not find user ${id}. Error: ${err}`);
+    }
+  }
+
+  async create(u: User): Promise<User> {
+    try {
+      const conn = await Client.connect();
+      const sql =
+        "INSERT INTO users (first_name, last_name, password) VALUES($1, $2, $3) RETURNING *";
+      const result = await conn.query(sql, [
+        u.first_name,
+        u.last_name,
+        u.password,
+      ]);
+      conn.release();
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(
+        `Could not create new user ${u.first_name}. Error: ${err}`
+      );
     }
   }
 }
