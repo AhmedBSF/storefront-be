11c8aaac7dfc39e1944df0e593154592192eb650
Ahmed <101567711+AhmedBSF@users.noreply.github.com>
2023-08-22 00:44:00 +0300

    add and use cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import config from "./config";
 
 const app: express.Application = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", function (req: Request, res: Response) {
