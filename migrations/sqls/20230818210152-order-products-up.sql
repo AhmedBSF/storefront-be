055ee8d53d0b3875100d22a44a91acfa4d6ddcc2
Ahmed <101567711+AhmedBSF@users.noreply.github.com>
2023-08-22 02:15:45 +0300

    update table

--- migrations/sqls/20230818210152-order-products-up.sql
+++ migrations/sqls/20230818210152-order-products-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE order_products(
     id SERIAL PRIMARY KEY,
-    products_id INTEGER REFERENCES products(id) NOT NULL,
+    product_id INTEGER REFERENCES products(id) NOT NULL,
     order_id INTEGER REFERENCES orders(id) NOT NULL,
     quantity INTEGER NOT NULL
 );
